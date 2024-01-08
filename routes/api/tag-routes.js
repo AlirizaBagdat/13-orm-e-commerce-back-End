cf0a51aea0a31f62d2588cdee6d20a0cf5a5bc4d
Aliriza Bagdat <ambagdat@gmail.com>
2024-01-08 23:50:22 +0000

    find by Pk

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,6 +19,14 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag }],
+    });
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
